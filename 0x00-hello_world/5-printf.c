cb5795316c183ef00a4737256149e1f69597273e
basheer42 <abdulazizbashir42@gmail.com>
2022-06-30 05:40:41 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
-
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -8,7 +7,7 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
